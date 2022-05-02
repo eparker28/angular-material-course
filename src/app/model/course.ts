20ec6cdc9ce58645b45ebfaefade1c8eb5ee7352
Erik <eparker28@hotmail.com>
2022-05-02 15:54:35 -0500

    Updating files in the tutorial

--- src/app/model/course.ts
+++ src/app/model/course.ts
@@ -1,11 +1,12 @@
 
 
 export interface Course {
-    id:number;
-    description:string;
+    id: number;
+    // The textual description of the course
+    description: string;
     iconUrl: string;
     courseListIcon: string;
     longDescription: string;
-    category:string;
-    lessonsCount:number;
+    category: string;
+    lessonsCount: number;
 }
--- src/app/model/lesson.ts
+++ src/app/model/lesson.ts
@@ -1,10 +1,16 @@
 
 
 export interface Lesson {
+    // The id number of the lesson
     id: number;
+    // The description of the lesson
     description: string;
+    // The length of the lesson
     duration: string;
+    // The order or sequence of the lesson
     seqNo: number;
+    // The id of the course to which the lesson belongs
     courseId: number;
+    // A lengthy description of the lesson's contents
     longDescription:string;
 }
